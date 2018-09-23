640ee507387b7ae0b118c27bc23e0fc76ca4c708
Jonathan Nicholas <jonicrecis@gmail.com>
2018-09-23 14:56:32 +1000

    1.5 Electric Boogaloo

--- threadtypes.py
+++ threadtypes.py
@@ -2,13 +2,11 @@ from subprocess import call as cl
 from time import sleep as sl
 
 def periodic_execution(command,next_execution_time):
-    print("PE is running")
-    print(command)
     while True:
         cl(command)
         sl(next_execution_time)
 
 def constant_execution(command):
-    print("CE is running")
     while True:
         cl(command)
+        sl(300)
