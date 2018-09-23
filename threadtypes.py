57c239df81d6ce61712947ee402eaa0fa5454d60
Jonathan Nicholas <jonicrecis@gmail.com>
2018-09-23 14:10:20 +1000

    1.2debug2

--- threadtypes.py
+++ threadtypes.py
@@ -3,6 +3,7 @@ from time import sleep as sl
 
 def periodic_execution(command,next_execution_time):
     print("PE is running")
+    print(command)
     while True:
         cl(command)
         sl(next_execution_time)
