900450e9e67a5925960ca868570ed74523169fc1
Jonathan Nicholas <jonicrecis@gmail.com>
2018-09-23 13:58:03 +1000

    1.2

--- __main__.py
+++ __main__.py
@@ -5,8 +5,8 @@ from threadtypes import periodic_execution as pe
 
 def main():
     threads = []
-    threads.append(Thread(target=ce,args=((spl("scripts/twitter_bot/env/bin/python3 threads/twitter_bot/__main__.py"),))))
-    threads.append(Thread(target=pe,args=((spl("bundle exec ruby scripts/ruby_calendar/main.rb"),),86400)))
+    threads.append(Thread(target=ce,args=((spl("scripts/twitter-bot/env/bin/python3 scripts/twitter-bot/__main__.py"),))))
+    threads.append(Thread(target=pe,args=((spl("bundle exec ruby scripts/ruby-calendar/main.rb"),),86400)))
     for thread in threads:
         thread.run()
 
