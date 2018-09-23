c84a611d2e6452816d298eaeb1322c8f5da6e43f
Jonathan Nicholas <jonicrecis@gmail.com>
2018-09-23 14:30:33 +1000

    1.3

--- __main__.py
+++ __main__.py
@@ -4,8 +4,8 @@ from threadtypes import constant_execution as ce
 from threadtypes import periodic_execution as pe
 
 def main():
-    Thread(target=pe,args=(spl("scripts/ruby-calendar/bin/bundle exec scripts/ruby-calendar/main.rb"),86400)).run()
-    Thread(target=ce,args=((spl("scripts/twitter-bot/env/bin/python3 scripts/twitter-bot/__main__.py"),))).run()
+    Thread(target=pe,args=(spl("scripts/ruby-calendar/bin/bundle exec scripts/ruby-calendar/main.rb"),86400)).start()
+    Thread(target=ce,args=((spl("scripts/twitter-bot/env/bin/python3 scripts/twitter-bot/__main__.py"),))).start()
 
 if __name__ == "__main__":
     main()
