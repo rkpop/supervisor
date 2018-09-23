0175c0b0dcea0c42a376dc3c495c2217c73be9ea
Jonathan Nicholas <jonicrecis@gmail.com>
2018-09-23 14:06:38 +1000

    1.2debug

--- threadtypes.py
+++ threadtypes.py
@@ -2,10 +2,12 @@ from subprocess import call as cl
 from time import sleep as sl
 
 def periodic_execution(command,next_execution_time):
+    print("PE is running")
     while True:
         cl(command)
         sl(next_execution_time)
 
 def constant_execution(command):
+    print("CE is running")
     while True:
         cl(command)
